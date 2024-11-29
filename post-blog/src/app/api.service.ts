c143260659e486120d67a342e89c013813ab5900
StelianY11 <134134591+StelianY11@users.noreply.github.com>
2024-11-29 15:42:28 +0200

    Add dinamic posts and themes
    anddcontect it to server side

--- post-blog/src/app/api.service.ts
+++ post-blog/src/app/api.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment.development';
 import { Post } from './types/post';
+import { Theme } from './types/theme';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,23 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
-  getPosts() {
+  getPosts(limit?: number) {
     const { apiUrl } = environment;
-    return this.http.get<Post>(`${apiUrl}/posts/.json`);
+
+    let url = `${apiUrl}/posts`;
+
+    if (limit) {
+      url += `?limit=${limit}`;
+    }
+
+    return this.http.get<Post[]>(url);
+  }
+
+  getThemes() {
+    const { apiUrl } = environment;
+
+    let url = `${apiUrl}/themes`;
+
+    return this.http.get<Theme[]>(url);
   }
 }
--- post-blog/src/app/theme-list/theme-list.component.ts
+++ post-blog/src/app/theme-list/theme-list.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Theme } from '../types/theme';
+import { LoaderComponent } from '../shared/loader/loader.component';
 
 @Component({
   selector: 'app-theme-list',
   standalone: true,
-  imports: [],
+  imports: [LoaderComponent],
   templateUrl: './theme-list.component.html',
   styleUrl: './theme-list.component.css'
 })
-export class ThemeListComponent {
+export class ThemeListComponent implements OnInit {
+  themes: Theme[] = [];
+  isLoading = true;
 
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.apiService.getThemes().subscribe((themes) => {
+      this.themes = themes;
+      this.isLoading = false;
+    });
+  };
 }
